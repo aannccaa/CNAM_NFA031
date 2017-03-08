d2e4aee8b338c28a9ba4f0e8ba852fb3661305ef
anca <aannccaa@gmail.com>
2017-03-08 21:49:54 +0100

    init

---
+++ src/Bonjour.java
@@ -0,0 +1,11 @@
+/* Premier programme
+-a enregistrer dans le fichier Bonjour.java
+-a compilar par javac Bonjour.java
+-a executer par java Bonjour
+*/
+public class Bonjour {
+public static void main (String[] args) {
+Terminal.ecrireStringln("Bonjour tout le monde !");
+System.out.println ("Hello everybody!");
+}
+}
---
+++ src/Conversion.java
@@ -0,0 +1,20 @@
+/* Un premier programme:
+-son nom est Conversion
+-il est sauvegarde dans le fichier Conversion.java
+-il est compile par javac Conversion.java
+-il est execute par java Conversion
+*/
+public class Conversion {
+public static void main (String[] args) {
+// Variables du programme
+double euros, francs;
+// Message pour la saisie
+Terminal.ecrireStringln("Entrez la somme en euros: ");
+// Lecture dans la variable euros
+euros = Terminal.lireDouble();
+// Calcul de la conversion
+francs = euros *6.559;
+// Affichage du resultat
+Terminal.ecrireStringln("La somme convertie en francs: "+ francs);
+}
+}
