82fdf1629fc02a5cc3c66d2657f54d07f12a2544
anca <aannccaa@gmail.com>
2017-03-12 23:10:38 +0100

    #

--- src/premEntiers.java
+++ src/premEntiers.java
@@ -1,9 +1,22 @@
+//////affiche les 10 premiers entiers en partant de 10 :
+//public class premEntiers {
+//	public static void main (String[] args) {
+//		for (int i=10; i>0; i=i-1) {
+//			Terminal.ecrireInt(i);
+//		Terminal.ecrireStringln(" . ");
+//		}
+//	}
+//}
+
+
 ////affiche les 10 premiers entiers en partant de 10 :
 public class premEntiers {
-	public static void main (String[] args) {
-		for (int i=10; i>0; i=i-1) {
-			Terminal.ecrireInt(i);
-		Terminal.ecrireStringln(" . ");
-		}
+public static void main (String[] args) {
+	int i=10;
+	while (i>0){	
+	Terminal.ecrireInt(i);
+	Terminal.ecrireStringln(" . ");
+	i=i-1;
 	}
+}
 }
