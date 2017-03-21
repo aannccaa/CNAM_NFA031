b3d78ea3785004ebcced953d341638ba89a022df
Anca <aannccaa@gmail.com>
2017-03-21 17:20:04 +0100

    update CalculsMultiplication

--- src/CalculsMultiplication.java
+++ src/CalculsMultiplication.java
@@ -12,14 +12,38 @@ par l’utilisateur. Par exemple, si le chiffre est 3, le programme affiche :
 9 x 3 = 27
 */
 
+//public class CalculsMultiplication {
+//	public static void main (String [] args) {
+//		int n, resultatMultiplication;
+//		Terminal.ecrireString("Entrez un entier:");
+//		n = Terminal.lireInt();
+//		for (int i=1; i<=9; i=i+1) {
+//		resultatMultiplication = i * n;
+//		Terminal.ecrireStringln(i + " x " + n + " = " + resultatMultiplication);
+//		}
+//	}
+//}
+
+/*
+Modifiez votre programme pour qu’il vérifie que le nombre entré
+par l’utilisateur est bien un chiffre (c’est à dire un nombre compris entre 1 et 9).
+*/
+
 public class CalculsMultiplication {
 	public static void main (String [] args) {
 		int n, resultatMultiplication;
-		Terminal.ecrireString("Entrez un entier:");
+		//boolean estChiffre=True;
+
+		Terminal.ecrireString("Entrez un chiffre de 1 à 9:");
 		n = Terminal.lireInt();
-		for (int i=1; i<=9; i=i+1) {
-		resultatMultiplication = i * n;
-		Terminal.ecrireStringln(i + " x " + n + " = " + resultatMultiplication);
+		if (n>0 && n<=9) {
+			for (int i=1; i<=9; i=i+1) {
+				resultatMultiplication = i * n;
+				Terminal.ecrireStringln(i + " x " + n + " = " + resultatMultiplication);
+			}
+		}
+		else {
+			Terminal.ecrireString("Le charactère introduit n'est pas un chiffre. Entrez un chiffre de 1 à 9:");
 		}
 	}
 }
